2a6512d2f8da69b822803f84f5c5fe53037f6276
Ferdinando <ferdinando.battillocchi@gmail.com>
2026-01-03 17:08:33 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- src/cs/Class1.cs
+++ src/cs/Class1.cs
@@ -1,4 +1,17 @@
-﻿using ExcelDna.Integration;
+/*
+ * PsicroAddIn - Libreria Psicrometrica per Excel
+ * Copyright (C) 2024  [Tuo Nome o Nome Utente GitHub]
+ * * Questo programma è software libero: è possibile ridistribuirlo e/o 
+ * modificarlo secondo i termini della licenza GNU General Public 
+ * License come pubblicata dalla Free Software Foundation, versione 3.
+ * * Questo programma è distribuito nella speranza che possa essere utile, 
+ * ma SENZA ALCUNA GARANZIA; senza neppure la garanzia implicita di 
+ * COMMERCIABILITÀ o IDONEITÀ PER UNO SCOPO PARTICOLARE. 
+ * Vedere la licenza GNU General Public License per ulteriori dettagli.
+ * * Dovresti aver ricevuto una copia della licenza GNU General Public License 
+ * insieme a questo programma. In caso contrario, vedi <https://www.gnu.org/licenses/>.
+ */
+using ExcelDna.Integration;
 using ExcelDna.Integration.CustomUI;
 using System;
 using System.Collections.Generic;
@@ -761,4 +774,5 @@ public class PsicroRibbon : ExcelRibbon
         // Richiama la funzione Info esistente
         PsicroAddIn.Psicro_Info();
     }
+
 }
